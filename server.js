0daadb0c5b1d41c46f9d4657bba53f00ce11d163
Sanjana Mathiyalagan <sanjuvimal@icloud.com>
2025-09-03 09:56:34 +0200

    removing ejs, node

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
-import express from "express";
 
 class Server {
     constructor() {
-        this.server = express();
         this.setupMiddleware();
         this.setupRoutes();
     }
 
     setupMiddleware() {
-        this.server.set("view engine", "ejs");
         this.server.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
-        this.server.use(express.static("public"));
-
     }
     setupRoutes() {
         this.server.get("/", (_, res) => {
